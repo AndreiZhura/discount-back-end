29e7789044de754384ffbccf79c55d86d90b8c7e
AndreiZhura <zhura2296@yandex.ru>
2023-07-18 14:27:55 +0300

    начал выдаваться токен

--- controllers/admins.js
+++ controllers/admins.js
@@ -1,45 +1,40 @@
 const bcrypt = require('bcryptjs');
 const Admin = require('../models/admin');
+const jwt = require('jsonwebtoken');
 
 module.exports.createAdmin = (req, res) => {
 
-    const { email, password, name } = req.body;
-    bcrypt.hash(password, 10)
-        .then((hash) => {
-            Admin.create({ email, password:hash, name })
-            .then((admin) => {
-                res.status(200)
-                    .send({ data: admin })
-            })
+  const { email, password, name } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      Admin.create({ email, password: hash, name })
+        .then((admin) => {
+          res.status(200)
+            .send({ data: admin })
         })
+    })
 
-        .catch((err) => {
-            res.status(400)
-                .send({ message: err.message });
-        })
+    .catch((err) => {
+      res.status(400)
+        .send({ message: err.message });
+    })
 
 };
 
-module.exports.login = (req,res) =>{
-    const {email, password} = req.body;
-    Admin.findOne({email})
-    .then((admin)=>{
-        if(!admin){
-          res.status(500).send("неправильно введены почта или пароль");
-        }
-        console.log(admin.password)
-        return bcrypt.compare(password, admin.password)
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  return Admin.findUserByCredentials(email, password)
+    .then((admin) => {
+      // создадим токен
+      const token = jwt.sign({ _id: admin._id }, 'some-secret-key');
+
+      // вернём токен
+      res.send({ token });
     })
-    .then((matched) => {
-        if (!matched) {
-          // хеши не совпали — отклоняем промис
-          return Promise.reject(new Error('Неправильные почта или пароль'));
-        }
-  
-        // аутентификация успешна
-        res.send({ message: 'Всё верно!' });
-      })
-      .catch((err) => {
-        res.status(401).send({ message: err.message });
-      });
-}
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
+    });
+}; 
--- models/admin.js
+++ models/admin.js
@@ -1,27 +1,46 @@
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
-const admin = new mongoose.Schema({
-    email:{
-        type:String,
-        require:true,
+const adminSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        require: true,
         minlenght: 2,
         maxlenght: 50,
-       
+
     },
-    password:{
-        type:String,
+    password: {
+        type: String,
         require: true,
     },
-    name:{
+    name: {
         type: String,
-        require:true,
+        require: true,
         minlenght: 2,
         maxlenght: 50,
     },
-    role:{
-        type:String,
+    role: {
+        type: String,
         default: 'admin'
     },
 })
 
-module.exports = mongoose.model('admins',admin);
+adminSchema.statics.findUserByCredentials = function(email, password){
+    return this.findOne({ email })
+      .then((user) => {
+        if (!user) {
+          return Promise.reject(new Error('Неправильные почта или пароль'));
+        }
+  
+        return bcrypt.compare(password, user.password)
+          .then((matched) => {
+            if (!matched) {
+              return Promise.reject(new Error('Неправильные почта или пароль'));
+            }
+  
+            return user;
+          });
+      });
+  };
+
+module.exports = mongoose.model('admins', adminSchema);
