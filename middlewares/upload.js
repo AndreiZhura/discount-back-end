0a39c72fc9812f2d5c7b16b0d594745986da0ecf
AndreiZhura <zhura2296@yandex.ru>
2023-07-30 12:27:53 +0300

    начал делать загрузку файлов

---
+++ middlewares/upload.js
@@ -0,0 +1,12 @@
+const multer = require('multer');
+const moment = require('moment');
+
+const storage = multer.diskStorage({
+    destination(req,res,cb){
+        
+    }
+})
+
+module.exports = multer({
+
+})
