bf66e9278b3475857636cb30f4d615c10688ac57
AndreiZhura <zhura2296@yandex.ru>
2023-08-04 12:04:33 +0300

    база данных работает на прием файлов

--- controllers/positions.js
+++ controllers/positions.js
@@ -2,16 +2,16 @@ const Position = require('../models/positions');
 
 module.exports.createPositions = (req, res) => {
 
-    //const image = req.files.image.map(value =>{return value.path;})
-    //const barcode = req.files.barcode.map(value =>{return value.path;})
+    const image = req.files.image.map(value =>{return value.path;})
+    const barcode = req.files.barcode.map(value =>{return value.path;})
 
     Position.create({
         name: req.body.name,
-        image: req.body.image,
+        image: req.files.image ? image: 'Такого файла нет',
         description: req.body.description,
         promocode: req.body.promocode,
         link: req.body.link,
-        barcode: req.body.barcode,
+        barcode: req.files.barcode ? barcode: 'Такого файла нет',
         date: req.body.date,
         category: req.body.category
     })
--- routers/positions.js
+++ routers/positions.js
@@ -1,15 +1,15 @@
 const positionRouter = require('express').Router();
 
 const { createPositions, getPositions, getPositionsId, deletePosition, updatePositionId } = require('../controllers/positions');
-//const upload = require('../middlewares/upload');
-//const imageAndBarcode = upload.fields([{name: 'image', maxCount: 1}, {name: 'barcode', maxCount: 1}]);
+const upload = require('../middlewares/upload');
+const imageAndBarcode = upload.fields([{name: 'image', maxCount: 1}, {name: 'barcode', maxCount: 1}]);
 
 positionRouter.get('/positions', getPositions);
 positionRouter.get('/positions/:_id', getPositionsId);
 
-positionRouter.post('/positions', createPositions);
+positionRouter.post('/positions',imageAndBarcode, createPositions);
 
-positionRouter.patch('/positions/:_id', updatePositionId);
+positionRouter.patch('/positions/:_id',  updatePositionId);
 positionRouter.delete('/positions/:_id', deletePosition);
 
 module.exports = positionRouter;
