0a916a6d61ee4773d0f5c8bf5bbeef2eb0082c9b
AndreiZhura <zhura2296@yandex.ru>
2023-08-16 22:31:24 +0300

    cors

--- app.js
+++ app.js
@@ -16,19 +16,20 @@ mongoose.connect('mongodb://localhost:27017/MyTestBackend')
 .then((res)=> console.log(`База данных подключекнна`))
 .catch((err)=> console.log(err))
 
-
 const options = {
-    origin: [
-      'https://localhost:10888',
-      'http://andreizhura.nomoredomains.club/',
-      'https://andreizhura.nomoredomains.club/'
-    ],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-    credentials: true,
-  };
+  origin: [
+    'https://api.andreizhura.nomoredomains.club',
+    'http://api.andreizhura.nomoredomains.club',
+    'https://andreizhura.nomoredomains.club',
+    'http://andreizhura.nomoredomains.club',
+    'https://10.200.61.46:10888',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/uploads/',express.static(path.join(__dirname, './uploads/')));
